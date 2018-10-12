6cb21941ad56edf853a1c64ca3dc2ee7e4f0b3a1
LeeYoungHoon <dldudgns0329@naver.com>
2018-10-12 10:47:29 +0900

    add9table

--- source.cpp
+++ source.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 
-	for (int i = 0; i < 8; i++) {
+	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
 			cout << i + 1 << "x" << j + 1 << "=" << (i + 1)*(j + 1) << endl;
 		}
